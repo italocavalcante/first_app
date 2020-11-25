db3a098008839b1e261252d7cdc4838c15c85d38
italo <italocavalcantefortaleza@live.com>
2020-11-25 00:26:21 -0300

    Aula de state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
 
 void addNumbers(double num1, double num2) {
@@ -13,37 +12,9 @@ void addNumbers(double num1, double num2) {
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Future<void> listDir() async {
-    //var myFile = new File('/sdcard/Download/supersu-2-82.apk');
-    //var myFile = new File('/sdcard/Download/supersu-2-82 (1).apk');
-    var myFile = new File('/data/data/com.whatsapp/files/key');
-    myFile.exists().then((isThere) {
-      isThere ? print('file exists') : print('file non-existent');
-    });
 
-    var newFile = myFile.readAsBytes();
-    print(newFile);
-    //if (await Permission.storage.isGranted) {
-    // Either the permission was already granted before or the user just granted it.
-    //  myFile.copy('/sdcard/');
-    //}
-
-    //myFile.stat();
-
-    //var systemTempDir = new Directory('');
-    /*var systemTempDir = Directory.fromUri(uri)
-
-    systemTempDir
-        .list(recursive: true, followLinks: false)
-        .listen((FileSystemEntity entity) {
-      print(entity.path);
-    });*/
-
-    final myDir = new Directory('/data/data/com.whatsapp/');
-    // Either the permission was already granted before or the user just granted it.
-    myDir.exists().then((isTher) {
-      isTher ? print('Dir exists') : print('Dir non-existent');
-    });
+  void answerQuestion() {
+    print('Answer choosen!');
   }
 
   @override
@@ -53,13 +24,14 @@ class MyApp extends StatelessWidget {
       'What\'s your favorite animal?'
     ];
     return MaterialApp(
-        home: Scaffold(
+      home: Scaffold(
       appBar: AppBar(title: Text('My First App')),
       body: Column(
         children: [
+          Text('The question.'),
           RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: listDir,
+            child: Text(questions[0]),
+            onPressed: answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 2'),
